3cf906cb5f528d9e13655e09dd9ae70ccdcb4fd0
BroCode-glithch <ariyomiracle1234@gmail.com>
2025-12-13 13:14:59 +0100

    Update api.ts

--- Frontend/src/lib/api.ts
+++ Frontend/src/lib/api.ts
@@ -197,19 +197,4 @@ export async function getCredentials(walletAddress: string): Promise<ApiResponse
   return res.json();
 }
 
-
-// export async function getCertificatesByWallet(
-//   wallet: string
-// ): Promise<ApiResponse<Certificate[]>> {
-//   return fetchWithFallback<Certificate[]>(
-//     `${API_BASE_URL}/credential/${wallet}`,
-//     { method: 'GET' },
-//     fallbackCertificates.map(cert => ({
-//       ...cert,
-//       recipientAddress: wallet
-//     }))
-//   );
-// }
-
-
 export { fallbackQuiz };
